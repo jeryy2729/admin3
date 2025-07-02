c5150cd9876da58d17971d7f0e1d547892252e83
jerry <jerry272961@gmail.com>
2025-07-02 19:55:41 +0500

    requests

---
+++ app/Http/Requests/Posts/CreateRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Posts;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+  public function rules(): array
+{
+    return [
+        'name' => 'required|string|max:255|unique:posts', // ✅ MUST HAVE THIS
+        'category_id' => 'required|exists:categories,id',
+        'tags' => 'required|array',
+        'tags.*' => 'exists:tags,id',
+        'description' => 'required|string',
+        'status' => 'boolean',
+    ];
+}
+
+
+
+        
+    }
+
